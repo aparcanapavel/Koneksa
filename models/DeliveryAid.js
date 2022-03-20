bd2cf555fc0d72c104db224820cfaa340cb47478
aparcanapavel <aparcanapavel@gmail.com>
2022-03-20 19:27:12 -0400

    deliveryAid.move() tested

--- models/DeliveryAid.js
+++ models/DeliveryAid.js
@@ -8,15 +8,53 @@ class DeliveryAid{
     return this.location;
   }
 
-  setLocation(coordinates){ 
+  setCoordinates(coordinates){ 
     // validate coordinates
     if(coordinates instanceof Array){
       this.location = coordinates; 
+      this.pizzasDelivered += 1;
 
       return true;
     } else{
       return false;
     }
+  }
+
+  move(direction){
+    if(!['^', 'v', '>', '<'].includes(direction)) return false;
+    
+    let newCoordinates = [];
+
+    switch (true) {
+      case direction === '^':
+        newCoordinates = [this.location[0], (this.location[1] + 1)];
+        break;
+
+      case direction === 'v':
+        newCoordinates = [this.location[0], (this.location[1] - 1)];
+        break;
+
+      case direction === '>':
+        newCoordinates = [(this.location[0] + 1), this.location[1]];
+        break;
+
+      case direction === '<':
+        newCoordinates = [(this.location[0] - 1), this.location[1]];
+        break;
+    
+      default:
+        break;
+    }
+
+    this.setCoordinates(newCoordinates);
+
+    return true;
+  }
+
+// for testing purposes
+  _reset(){
+    this.pizzasDelivered = 1;
+    this.location = [0,0];
   }
 
 }
--- tests/deliveryAid.test.js
+++ tests/deliveryAid.test.js
@@ -20,21 +20,71 @@ const deliveryAidTests = () => {
       });
     });
 
-    describe('setLocation()', () => {
+    describe('setCoordinates()', () => {
       it('should return true if parameter is of type array', () => {
-        assert.equal(deliveryAid.setLocation([0,0]), true);
+        assert.equal(deliveryAid.setCoordinates([0,0]), true);
       });
       it('should return false if parameter is not an array', () => {
-        assert.equal(deliveryAid.setLocation('[0,0]'), false);
+        assert.equal(deliveryAid.setCoordinates('[0,0]'), false);
       });
       it('should set a new location', () => {
-        deliveryAid.setLocation([0,1]);
+        deliveryAid.setCoordinates([0,1]);
         assert.deepEqual(deliveryAid.getLocation(), [0,1]);
         assert.notDeepEqual(deliveryAid.getLocation(), [0,0]);
       });
+      it('should update pizzas delivered by 1', () => {
+        deliveryAid.setCoordinates([0,0]);
+        deliveryAid.pizzasDelivered = 1;
+
+        deliveryAid.setCoordinates([0,0]);
+      
+        assert.equal(deliveryAid.pizzasDelivered, 2);
+      });
     });
 
-    
+    describe('move()', () => {
+      beforeEach(() => {
+        deliveryAid.setCoordinates([0,0]);
+        deliveryAid.pizzasDelivered = 1;
+      });
+      it('should return early if move direction is invalid', () => {
+        assert.equal(deliveryAid.move('+'), false);
+      });
+      it('should return true if move is valid', () => {
+        assert.equal(deliveryAid.move('v'), true);
+      });
+      it('should move delivery aid right on ">"', () => {
+        deliveryAid.move('>');
+
+        assert.deepEqual(deliveryAid.getLocation(), [1,0]);
+      });
+      it('should move delivery aid down on "v"', () => {
+        deliveryAid.move('v');
+
+        assert.deepEqual(deliveryAid.getLocation(), [0,-1]);
+      });
+      it('should move delivery aid left on "<"', () => {
+        deliveryAid._reset();
+        deliveryAid.move('<');
+
+        assert.deepEqual(deliveryAid.getLocation(), [-1, 0]);
+      });
+      it('should move delivery aid up on "^"', () => {
+        deliveryAid.move('^');
+
+        assert.deepEqual(deliveryAid.getLocation(), [0,1]);
+      });
+    });
+
+    describe('mocha before hooks', function () {
+      before(() => console.log('*** top-level before()'));
+      beforeEach(() => console.log('*** top-level beforeEach()'));
+      describe('nesting', function () {
+        before(() => console.log('*** nested before()'));
+        beforeEach(() => console.log('*** nested beforeEach()'));
+        it('is a nested spec', () => true);
+      });
+    });
     
   });
 }
